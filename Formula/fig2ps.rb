ffc362123519657d64173badfdc969034c471da7
Weizhao Ouyang <o451686892@gmail.com>
2024-06-03 12:05:27 +0000

    fig2ps: add 1.5 bottle.

--- Formula/fig2ps.rb
+++ Formula/fig2ps.rb
@@ -5,6 +5,11 @@ class Fig2ps < Formula
   sha256 "e59dece4106beb08236e6c4c5827e0ad8a5ed0276d3444676019e19ebd0c49b9"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/ownia/homebrew-ownia/releases/download/fig2ps-1.5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f2990690cdf79ac1435c1a3136edf34d060f0c116bf9cae5a11595bd4e361ac4"
+  end
+
   depends_on "fig2dev"
 
   def install
