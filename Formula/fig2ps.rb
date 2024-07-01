4b7eb688df8e7ddc688fa6e26f3491e16960a725
Weizhao Ouyang <o451686892@gmail.com>
2024-07-01 22:24:46 +0800

    test: test workflow
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- Formula/fig2ps.rb
+++ Formula/fig2ps.rb
@@ -1,5 +1,5 @@
 class Fig2ps < Formula
-  desc "Perl script designed to convert Xfig files to postscript or PDF files"
+  desc "Perl script designed to convert Xfig files to postscript or PDF files - test"
   homepage "https://sourceforge.net/projects/fig2ps"
   url "https://downloads.sourceforge.net/project/fig2ps/fig2ps-1.5.tar.bz2"
   sha256 "e59dece4106beb08236e6c4c5827e0ad8a5ed0276d3444676019e19ebd0c49b9"
