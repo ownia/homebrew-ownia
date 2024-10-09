1dbe53c7751143b6fff6473e8ca4744179e1a190
Weizhao Ouyang <o451686892@gmail.com>
2024-10-10 00:11:58 +0800

    so-novel 1.6.0

--- Formula/so-novel.rb
+++ Formula/so-novel.rb
@@ -1,19 +1,17 @@
 class SoNovel < Formula
   desc "Novel download tool"
   homepage "https://github.com/freeok/so-novel"
-  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.5.9.tar.gz"
-  sha256 "3cb684198e351e6bb8859c41df7bf1227b56bad58dafb205bec0711d3402a3ee"
+  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "f48619a56f57312978eb344ef8af76b8ef0bf3960ca0749dfa2f7fafd496339e"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/ownia/homebrew-ownia/releases/download/so-novel-1.5.9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "81140659af8a4b1b171d753d5da34fd5b81087b7515596457572e5502bb57fe9"
-  end
-
   depends_on "maven" => :build
-  depends_on "openjdk" => :build
+  depends_on "openjdk@17" => :build
 
   def install
+    # ENV["JAVA_HOME"] = "/opt/homebrew/opt/openjdk@17/"
+    ENV["JAVA_HOME"] = Formula["openjdk@17"].opt_prefix
+    # ENV["PATH"] = "$JAVA_HOME/bin:$PATH"
     system "mvn", "clean", "package", "-DskipTests"
     cp "config.ini", "#{prefix}/config.ini"
     cp "target/app-jar-with-dependencies.jar", "#{prefix}/app.jar"
