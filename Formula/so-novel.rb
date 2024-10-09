3475fa636c0be812cd991b02e61e370afadcb5c6
Weizhao Ouyang <o451686892@gmail.com>
2024-10-09 16:40:16 +0000

    so-novel: add 1.6.0 bottle.

--- Formula/so-novel.rb
+++ Formula/so-novel.rb
@@ -5,6 +5,11 @@ class SoNovel < Formula
   sha256 "f48619a56f57312978eb344ef8af76b8ef0bf3960ca0749dfa2f7fafd496339e"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/ownia/homebrew-ownia/releases/download/so-novel-1.6.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "3a2beade461ffce07ae766240c4b57a9c1de7dc6c40e8bd04480029b3b5979c1"
+  end
+
   depends_on "maven" => :build
   depends_on "openjdk@17" => :build
 
