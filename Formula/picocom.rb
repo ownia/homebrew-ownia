6aa9846d6ac6263e7082c6489505294df18cba0a
Weizhao Ouyang <o451686892@gmail.com>
2024-10-10 13:18:04 +0000

    picocom: add 2024-07 bottle.

--- Formula/picocom.rb
+++ Formula/picocom.rb
@@ -6,6 +6,11 @@ class Picocom < Formula
   sha256 "4379de2ec591a5848123f37ccdbc7fbeee6dd3520ef1ce4119d84202fc268a17"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/ownia/homebrew-ownia/releases/download/picocom-2024-07"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f7a4928585ddce3fbc1c18d72c6dc5c70efdc8cd6c3aa7d3f16cb2aef80bb88c"
+  end
+
   depends_on "go-md2man" => :build
 
   def install
