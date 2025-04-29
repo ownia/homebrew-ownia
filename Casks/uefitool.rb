0103b993071ba028aafea23cc82ae537dc34852f
Weizhao Ouyang <o451686892@gmail.com>
2025-04-29 22:30:30 +0800

    uefitool: bump to A71
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- Casks/uefitool.rb
+++ Casks/uefitool.rb
@@ -1,13 +1,11 @@
 cask "uefitool" do
-  version "70"
-  sha256 "f1f73e5712f3e1f4a620bbe2fdf1fb50ff42e59768b738a8a1dd321a8c7b2f18"
+  version "71"
+  sha256 "8475df3cd94ab798b2cac286b0daf578326244a749d29eab93a6a86c881bb3eb"
 
-  url "https://github.com/LongSoft/UEFITool/releases/download/A#{version}/UEFITool_NE_A#{version}_universal_mac.zip"
+  url "https://github.com/LongSoft/UEFITool/releases/download/A#{version}/UEFITool_NE_A#{version}_universal_mac.dmg"
   name "UEFITool"
   desc "UEFI firmware image viewer and editor"
   homepage "https://github.com/LongSoft/UEFITool"
-
-  container type: :zip
 
   app "UEFITool.app"
 end
