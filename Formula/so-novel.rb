6c77942ea5823b4ef5805e34cf4b997f500a769c
Weizhao Ouyang <o451686892@gmail.com>
2025-05-08 20:17:41 +0800

    so-novel 1.8.2
    Closes #45.
    
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- Formula/so-novel.rb
+++ Formula/so-novel.rb
@@ -1,10 +1,9 @@
 class SoNovel < Formula
   desc "Novel download tool"
   homepage "https://github.com/freeok/so-novel"
-  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.8.1.tar.gz"
-  sha256 "6fda6d416865fb7f1d909ac521f22641bbe8e4dc487ab5aec2b05eb87b8fb52b"
+  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.8.2.tar.gz"
+  sha256 "bd4f9d49b207c434b9fc9a7c331d517f2cee857216bd47f0d305d8bfa94d843b"
   license "AGPL-3.0-only"
-  revision 1
 
   bottle do
     root_url "https://github.com/ownia/homebrew-ownia/releases/download/so-novel-1.8.1_1"
@@ -14,9 +13,6 @@ class SoNovel < Formula
 
   depends_on "maven" => :build
   depends_on "openjdk@17" => :build
-
-  # temp fix
-  patch :DATA
 
   def install
     # ENV["JAVA_HOME"] = "/opt/homebrew/opt/openjdk@17/"
@@ -35,56 +31,3 @@ class SoNovel < Formula
     assert shell_output("test -x #{bin}/so-novel")
   end
 end
-
-__END__
-From 567e98f446bfe9991a607c1019bde6cebaf51334 Mon Sep 17 00:00:00 2001
-From: Weizhao Ouyang <o451686892@gmail.com>
-Date: Wed, 7 May 2025 23:08:56 +0800
-Subject: [PATCH] :bug: fix watchConfig NoResourceException
-
-Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>
----
- src/main/java/com/pcdd/sonovel/Main.java | 12 ++++++++++--
- 1 file changed, 10 insertions(+), 2 deletions(-)
-
-diff --git a/src/main/java/com/pcdd/sonovel/Main.java b/src/main/java/com/pcdd/sonovel/Main.java
-index 10a219a..d356674 100644
---- a/src/main/java/com/pcdd/sonovel/Main.java
-+++ b/src/main/java/com/pcdd/sonovel/Main.java
-@@ -1,5 +1,6 @@
- package com.pcdd.sonovel;
- 
-+import cn.hutool.core.io.FileUtil;
- import cn.hutool.core.io.resource.ResourceUtil;
- import cn.hutool.core.lang.Console;
- import cn.hutool.core.lang.ConsoleTable;
-@@ -27,6 +28,7 @@
- import java.io.File;
- import java.util.List;
- import java.util.Scanner;
-+import java.nio.file.Paths;
- 
- import static org.fusesource.jansi.AnsiRenderer.render;
- 
-@@ -198,7 +200,13 @@ private static void printHint() {
-     }
- 
-     private static void watchConfig() {
--        String path = System.getProperty("user.dir") + File.separator + ConfigUtils.resolveConfigFileName();
-+        String path;
-+        String configFilePath = System.getProperty("config.file");
-+        if (!FileUtil.exist(configFilePath)) {
-+            path = System.getProperty("user.dir") + File.separator + ConfigUtils.resolveConfigFileName();
-+        } else {
-+            path = Paths.get(configFilePath).toAbsolutePath().toString();
-+        }
-         Setting setting = new Setting(path);
-         // 监听配置文件
-         setting.autoLoad(true, aBoolean -> {
-@@ -208,4 +216,4 @@ private static void watchConfig() {
-         });
-     }
- 
--}
-\ No newline at end of file
-+}
