7a70d8d95a1584f3dd9edf9c4875a16f0e4decd7
Weizhao Ouyang <o451686892@gmail.com>
2025-05-08 12:26:21 +0000

    so-novel: update 1.8.2 bottle.

--- Formula/so-novel.rb
+++ Formula/so-novel.rb
@@ -6,9 +6,8 @@ class SoNovel < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    root_url "https://github.com/ownia/homebrew-ownia/releases/download/so-novel-1.8.1_1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9a1745dc277cd1123173865e1b158800cd5d58b5259efdff1f1ea3c42d42580a"
+    root_url "https://github.com/ownia/homebrew-ownia/releases/download/so-novel-1.8.2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9f670544a0d0d469bf15b0191c7ac17f125939ba588964ce6290513af9577d9d"
   end
 
   depends_on "maven" => :build
