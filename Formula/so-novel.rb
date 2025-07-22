80cc204abab0b5697e1853784e844875db1a1416
Weizhao Ouyang <o451686892@gmail.com>
2025-07-22 19:39:29 +0800

    so-novel 1.8.5
    Closes #52.
    
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- Formula/so-novel.rb
+++ Formula/so-novel.rb
@@ -1,10 +1,9 @@
 class SoNovel < Formula
   desc "Novel download tool"
   homepage "https://github.com/freeok/so-novel"
-  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.8.4.tar.gz"
-  sha256 "491b234fb7c7b167964cf76b385096db55d1def9f50da091d3a4d58e0d8e7541"
+  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.8.5.tar.gz"
+  sha256 "fa02da24ea54a123cf8187c3e1020b40ce806527ef0ff194214f8ac2d7fdfd6d"
   license "AGPL-3.0-only"
-  revision 2
 
   bottle do
     root_url "https://github.com/ownia/homebrew-ownia/releases/download/so-novel-1.8.4_2"
@@ -18,18 +17,14 @@ class SoNovel < Formula
     # ENV["JAVA_HOME"] = "/opt/homebrew/opt/openjdk@17/"
     ENV["JAVA_HOME"] = Formula["openjdk@17"].opt_prefix
     # ENV["PATH"] = "$JAVA_HOME/bin:$PATH"
-    system "mvn", "clean", "package", "-Dmaven.test.skip=true"
+    system "mvn", "clean", "package", "-Dmaven.test.skip=true", "-DjrePath=runtime"
     cp "bundle/rules/main-rules.json", "#{prefix}/main-rules.json"
     cp "config.ini", "#{prefix}/config.ini"
-    # inreplace "#{prefix}/config.ini", /^active-rules\s*=\s*.*$/, "active-rules = #{prefix}/main-rules.json"
+    inreplace "#{prefix}/config.ini", /^active-rules\s*=\s*.*$/, "active-rules = #{prefix}/main-rules.json"
     cp "target/app-jar-with-dependencies.jar", "#{prefix}/app.jar"
     java = Formula["openjdk@17"].opt_prefix/"bin/java"
     (prefix/"bin/so-novel").write <<~EOS
       #!/bin/bash
-      TMP_RULES="$(pwd)/rules"
-      mkdir -p "$TMP_RULES"
-      cp "#{prefix}/main-rules.json" "$TMP_RULES/"
-      trap 'rm -rf "$TMP_RULES"' EXIT
       #{java} -Dconfig.file=#{prefix}/config.ini -Denv=prod -jar #{prefix}/app.jar
     EOS
   end
