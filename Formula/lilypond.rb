25832d8299e44465a447a851bc5c12f9cc4b537c
Weizhao Ouyang <o451686892@gmail.com>
2025-09-11 22:24:37 +0800

    lilypond: add lilypond development release
    Closes #57.
    
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

---
+++ Formula/lilypond.rb
@@ -0,0 +1,96 @@
+class Lilypond < Formula
+  desc "Music engraving system"
+  homepage "https://lilypond.org"
+  url "https://gitlab.com/lilypond/lilypond/-/archive/v2.25.28/lilypond-v2.25.28.tar.gz"
+  sha256 "ded118926a4d995c3d9edd96b4199ee6ccea4fb480bdd90cb5ef98658982c9e4"
+  license all_of: [
+    "GPL-3.0-or-later",
+    "GPL-3.0-only",
+    "OFL-1.1-RFN",
+    "GFDL-1.3-no-invariants-or-later",
+    :public_domain,
+    "MIT",
+    "AGPL-3.0-only",
+    "LPPL-1.3c",
+  ]
+
+  depends_on "autoconf" => :build
+  depends_on "bison" => :build # bison >= 2.4.1 is required
+  depends_on "fontforge" => :build
+  depends_on "make" => :build
+  depends_on "pkgconf" => :build
+  depends_on "t1utils" => :build
+  depends_on "texinfo" => :build # makeinfo >= 6.1 is required
+  depends_on "texlive" => :build
+  depends_on "bdw-gc"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "gettext"
+  depends_on "ghostscript"
+  depends_on "glib"
+  depends_on "guile"
+  depends_on "harfbuzz"
+  depends_on "pango"
+  depends_on "python@3.13"
+
+  uses_from_macos "flex" => :build
+  uses_from_macos "perl" => :build
+
+  resource "font-urw-base35" do
+    url "https://github.com/ArtifexSoftware/urw-base35-fonts/archive/refs/tags/20200910.tar.gz"
+    sha256 "e0d9b7f11885fdfdc4987f06b2aa0565ad2a4af52b22e5ebf79e1a98abd0ae2f"
+  end
+
+  def install
+    system "./autogen.sh", "--noconfigure"
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--datadir=#{share}",
+                          "--disable-documentation",
+                          "GUILE_FLAVOR=guile-3.0"
+
+    system "make"
+    system "make", "install"
+
+    system "make", "bytecode"
+    system "make", "install-bytecode"
+
+    elisp.install share.glob("emacs/site-lisp/*.el")
+
+    fonts = pkgshare/(build.head? ? File.read("out/VERSION").chomp : version)/"fonts/otf"
+
+    resource("font-urw-base35").stage do
+      ["C059", "NimbusMonoPS", "NimbusSans"].each do |name|
+        Dir["fonts/#{name}-*.otf"].each do |font|
+          fonts.install font
+        end
+      end
+    end
+
+    ["cursor", "heros", "schola"].each do |name|
+      cp Dir[Formula["texlive"].share/"texmf-dist/fonts/opentype/public/tex-gyre/texgyre#{name}-*.otf"], fonts
+    end
+  end
+
+  test do
+    (testpath/"test.ly").write "\\relative { c' d e f g a b c }"
+    system bin/"lilypond", "--loglevel=ERROR", "test.ly"
+    assert_path_exists testpath/"test.pdf"
+
+    output = shell_output("#{bin}/lilypond --define-default=show-available-fonts 2>&1")
+             .encode("UTF-8", invalid: :replace, replace: "\ufffd")
+    common_styles = ["Regular", "Bold", "Italic", "Bold Italic"]
+    {
+      "C059"            => ["Roman", *common_styles[1..]],
+      "Nimbus Mono PS"  => common_styles,
+      "Nimbus Sans"     => common_styles,
+      "TeX Gyre Cursor" => common_styles,
+      "TeX Gyre Heros"  => common_styles,
+      "TeX Gyre Schola" => common_styles,
+    }.each do |family, styles|
+      styles.each do |style|
+        assert_match(/^\s*#{family}:style=#{style}$/, output)
+      end
+    end
+  end
+end
