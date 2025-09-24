03535df9302da88832f66c42e47bb16f70a65604
Weizhao Ouyang <o451686892@gmail.com>
2025-09-24 19:59:02 +0800

    so-novel 1.9.3
    Closes #59.
    
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- Formula/so-novel.rb
+++ Formula/so-novel.rb
@@ -1,8 +1,8 @@
 class SoNovel < Formula
   desc "Novel download tool"
   homepage "https://github.com/freeok/so-novel"
-  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.9.2.tar.gz"
-  sha256 "25f4f02214561aad04a6264881abb8bb8ccbe2d27cc42404443db0d562cdfac1"
+  url "https://github.com/freeok/so-novel/archive/refs/tags/v1.9.3.tar.gz"
+  sha256 "7ec7fe71d1999f1404dc54ab37c668d2345fb50745dbb5ce75c7c635ba04e3d3"
   license "AGPL-3.0-only"
 
   bottle do
@@ -11,19 +11,16 @@ class SoNovel < Formula
   end
 
   depends_on "maven"
-  depends_on "openjdk@17"
+  depends_on "openjdk@21"
 
   def install
-    # ENV["JAVA_HOME"] = "/opt/homebrew/opt/openjdk@17/"
-    ENV["JAVA_HOME"] = Formula["openjdk@17"].opt_prefix
-    # ENV["PATH"] = "$JAVA_HOME/bin:$PATH"
+    ENV["JAVA_HOME"] = Formula["openjdk@21"].opt_prefix
     system "mvn", "clean", "package", "-Dmaven.test.skip=true", "-DjrePath=runtime"
     cp "bundle/rules/main-rules.json", "#{prefix}/main-rules.json"
     cp "bundle/config.ini", "#{prefix}/config.ini"
     inreplace "#{prefix}/config.ini", /^active-rules\s*=\s*.*$/, "active-rules = #{prefix}/main-rules.json"
     cp "target/app-jar-with-dependencies.jar", "#{prefix}/app.jar"
-    java = Formula["openjdk@17"].opt_prefix/"bin/java"
-    # cli mode can fallback to tui mode
+    java = Formula["openjdk@21"].opt_prefix/"bin/java"
     (prefix/"bin/so-novel").write <<~EOS
       #!/bin/bash
       #{java} -Dconfig.file=#{prefix}/config.ini -Dmode=tui -jar #{prefix}/app.jar "$@"
