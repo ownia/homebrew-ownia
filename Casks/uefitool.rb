609aa50ca3141423c6df0fca9ab9e3e50fab5171
Weizhao Ouyang <o451686892@gmail.com>
2026-01-26 08:51:42 +0000

    uefitool 73

--- Casks/uefitool.rb
+++ Casks/uefitool.rb
@@ -1,13 +1,13 @@
 cask "uefitool" do
-  version "72"
-  sha256 "af849fe60ddc3fd8d26c38c4ec5eb6eeda8e2cbb1534f289249559f283804c44"
-
-  depends_on macos: :big_sur
+  version "73"
+  sha256 "508ffe1de4e45098576b873c3c5fc61356cdb8e348bc5767caa7aa2c8d7fc56e"
 
   url "https://github.com/LongSoft/UEFITool/releases/download/A#{version}/UEFITool_NE_A#{version}_universal_mac.dmg"
   name "UEFITool"
   desc "UEFI firmware image viewer and editor"
   homepage "https://github.com/LongSoft/UEFITool"
+
+  depends_on macos: :big_sur
 
   app "UEFITool.app"
 end
