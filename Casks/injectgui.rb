e4ae109626fdf20aad73b89acb1f2b38b572c2d4
Weizhao Ouyang <weizhao.ouyang@arm.com>
2026-01-26 16:46:39 +0800

    uefitool: add depends_on
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- Casks/injectgui.rb
+++ Casks/injectgui.rb
@@ -2,6 +2,8 @@ cask "injectgui" do
   version "1.2.3"
   sha256 "1c3043b79303372f1fef5407eed885a30318a0fb5d91a38df4510ebd5804d8d8"
 
+  depends_on macos: :big_sur
+
   url "https://github.com/wibus-wee/InjectGUI/releases/download/v#{version}/InjectGUI.dmg"
   name "InjectGUI"
   desc "Integrated Injection Framework (GUI version)"
--- Casks/uefitool.rb
+++ Casks/uefitool.rb
@@ -2,6 +2,8 @@ cask "uefitool" do
   version "72"
   sha256 "af849fe60ddc3fd8d26c38c4ec5eb6eeda8e2cbb1534f289249559f283804c44"
 
+  depends_on macos: :big_sur
+
   url "https://github.com/LongSoft/UEFITool/releases/download/A#{version}/UEFITool_NE_A#{version}_universal_mac.dmg"
   name "UEFITool"
   desc "UEFI firmware image viewer and editor"
