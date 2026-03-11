fba09e8bcfd22468db528b03dfb5ccc3a76055fe
Weizhao Ouyang <o451686892@gmail.com>
2026-03-11 18:12:04 +0800

    so-novel: bump revision for hot fix
    Closes #76.
    
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- Formula/so-novel.rb
+++ Formula/so-novel.rb
@@ -4,6 +4,7 @@ class SoNovel < Formula
   url "https://github.com/freeok/so-novel/archive/refs/tags/v1.10.0.tar.gz"
   sha256 "90598643e8457664e71a5eb795e2a58e915bb28572876486e33c5ecb4407708b"
   license "AGPL-3.0-only"
+  revision 1
 
   bottle do
     root_url "https://github.com/ownia/homebrew-ownia/releases/download/so-novel-1.10.0"
@@ -16,9 +17,9 @@ class SoNovel < Formula
   def install
     ENV["JAVA_HOME"] = Formula["openjdk@21"].opt_prefix
     system "mvn", "clean", "package", "-Dmaven.test.skip=true", "-DjrePath=runtime"
-    cp "bundle/rules/main.json", "#{prefix}/main-rules.json"
+    cp "bundle/rules/main.json", "#{prefix}/main.json"
     cp "bundle/config.ini", "#{prefix}/config.ini"
-    inreplace "#{prefix}/config.ini", /^active-rules\s*=\s*.*$/, "active-rules = #{prefix}/main-rules.json"
+    inreplace "#{prefix}/config.ini", /^active-rules\s*=\s*.*$/, "active-rules = #{prefix}/main.json"
     cp "target/app-jar-with-dependencies.jar", "#{prefix}/app.jar"
     java = Formula["openjdk@21"].opt_prefix/"bin/java"
     (prefix/"bin/so-novel").write <<~EOS
