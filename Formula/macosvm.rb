8b4beb15d3c1294e3fe76d6dc4f6963b95f41eae
Weizhao Ouyang <ownia.ouyang@gmail.com>
2026-06-24 10:39:03 +0800

    macosvm: update url
    Signed-off-by: Weizhao Ouyang <ownia.ouyang@gmail.com>

--- Formula/macosvm.rb
+++ Formula/macosvm.rb
@@ -1,8 +1,7 @@
 class Macosvm < Formula
   desc "Tool for running macOS guest virtual machines"
   homepage "https://github.com/s-u/macosvm"
-  url "https://github.com/s-u/macosvm/archive/641c3cf407bd04a429735a40b4ba2dcc008b813a.tar.gz"
-  version "0.2-2"
+  url "https://github.com/s-u/macosvm/archive/refs/tags/0.2-2.tar.gz"
   sha256 "6753c90abe0d0da5fe0506ac34a31ae9ab3633f8af1c9bdd7b4b451a69aefe05"
   license "GPL-2.0-or-later"
 
